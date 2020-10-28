7e316519e0c27d091ed67d894d39b9ea64a7ecf5
Jack Wong <jackwong.professional@gmail.com>
2020-10-28 18:34:57 -0400

    Initial Commit

---
+++ LocationAlarm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocationAlarm
+//
+//  Created by Jack Wong on 10/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocationAlarm/LocationAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationAlarmApp.swift
+//  LocationAlarm
+//
+//  Created by Jack Wong on 10/28/20.
+//
+
+import SwiftUI
+
+@main
+struct LocationAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
